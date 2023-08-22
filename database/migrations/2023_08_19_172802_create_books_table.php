ace918e08b8accc3859838e0d10aefe93171e75a
XediceYusifova <yxedice1@gmail.com>
2023-08-22 15:33:19 +0400

    install laravel and add migrations

---
+++ database/migrations/2023_08_19_172802_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('selflink');
+            $table->integer('writer_id');
+            $table->integer('publisher_id');
+            $table->string('image');
+            $table->double('price');
+            $table->text('description')->nullable();
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
